dcf9c7fdae3c66687eaff21e691587aac412bc84
Daniel Jacob Martin <djm24862@gmail.com>
2021-01-27 22:33:52 -0500

    https docker registry

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in $(cat image-list.txt); do 
 	docker pull $i
-	docker tag $i base:5000/$i
-	docker push base:5000/$i
+	docker tag $i localhost:5000/$i
+	docker push localhost:5000/$i
 done
